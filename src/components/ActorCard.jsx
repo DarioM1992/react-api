43f5d02213c338a7e598c72f8edd8eb12d1014b0
DarioM1992 <dario21992@gmail.com>
2026-08-19 17:19:48 +0200

    risolto problema immagini non caricate

--- src/components/ActorCard.jsx
+++ src/components/ActorCard.jsx
@@ -3,7 +3,7 @@ import './ActorCard.css';
 export const ActorCard = props => (
    <div className="actor-card">
     <div className='pic'>
-        <img src="{props.actor.image}" alt="" />
+        <img src={props.actor.image} alt={props.actor.name} />
         </div>
     <div>
         <h2>{props.actor.name}</h2>
